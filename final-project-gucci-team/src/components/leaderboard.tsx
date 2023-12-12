961f4e19da363210a8a29c53e2e27715109735b8
sharma-mitneu <145163557+sharma-mitneu@users.noreply.github.com>
2023-12-11 19:19:00 -0500

    made changes in esx files and removed unused files

--- final-project-gucci-team/src/components/leaderboard.tsx
+++ final-project-gucci-team/src/components/leaderboard.tsx
@@ -1,18 +1,51 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import '../CSS/Leaderboard.css'
 import Table from './Table';
 
 const Leaderboard: React.FC = () => {
   const [selectedUser, setSelectedUser] = useState<string | null>(null);
 
-  const handleRowClick = (username: string) => {
-    setSelectedUser(username);
-  };
+  useEffect(() => {
+    const calculateScore = (problemsSolved: number) => {
+      return problemsSolved * 3;
+    };
+
+    const updateScores = () => {
+      const rows = document.querySelectorAll('.table tbody tr');
+
+      rows.forEach((row: any) => {
+        const problemsSolved = parseInt(row.cells[2].innerText, 10);
+        const score = calculateScore(problemsSolved);
+        row.cells[3].innerText = score;
+      });
+    };
+
+    // Attach a click event listener to each row to show scores and problems solved
+    const rows = document.querySelectorAll('.table tbody tr');
+    rows.forEach((row: any) => {
+      row.addEventListener('click', () => {
+        const username = row.dataset.username;
+        setSelectedUser(username);
+      });
+    });
+
+    // Close the nested list when clicking outside of it
+    document.addEventListener('click', (event) => {
+      const nestedListContainer = document.getElementById('nestedListContainer');
+      if (!nestedListContainer!.contains(event.target) && !rows.contains(event.target)) {
+        nestedListContainer!.style.display = 'none';
+      }
+    });
+
+    // Initial update of scores
+    updateScores();
+  }, []);
 
   return (
     <div className="container mt-3">
       <h2>Code Competition Leaderboard</h2>
       <div className="leaderboard-container">
-        <Table onRowClick={handleRowClick} selectedUser={selectedUser} />
+        <Table onRowClick={(username) => setSelectedUser(username)} selectedUser={selectedUser} />
       </div>
     </div>
   );
--- final-project-gucci-team/src/components/table.tsx
+++ final-project-gucci-team/src/components/table.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import '../CSS/Table.css'
 import NestedList from './NestedList';
 
 interface TableProps {
@@ -7,14 +8,28 @@ interface TableProps {
 }
 
 const Table: React.FC<TableProps> = ({ onRowClick, selectedUser }) => {
-  const [users] = useState([
+  const [users, setUsers] = useState([
     { rank: 1, username: 'JohnDoe', problemsSolved: 10 },
     { rank: 2, username: 'JaneSmith', problemsSolved: 8 },
     // Add more users as needed
   ]);
 
+  useEffect(() => {
+    // Update scores when the component mounts
+    updateScores();
+  }, []);
+
   const calculateScore = (problemsSolved: number) => {
     return problemsSolved * 3;
+  };
+
+  const updateScores = () => {
+    setUsers((prevUsers) =>
+      prevUsers.map((user) => ({
+        ...user,
+        score: calculateScore(user.problemsSolved),
+      }))
+    );
   };
 
   return (
@@ -38,7 +53,7 @@ const Table: React.FC<TableProps> = ({ onRowClick, selectedUser }) => {
               <td>{user.rank}</td>
               <td>{user.username}</td>
               <td>{user.problemsSolved}</td>
-              <td>{calculateScore(user.problemsSolved)}</td>
+              {/* <td>{user.score}</td> */}
             </tr>
           ))}
         </tbody>
